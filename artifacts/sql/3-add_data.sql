f7b0219356bd5ba46fd56aa89c5f1830ddf84fb0
Admin <rbroker@digital.ai>
2020-12-18 16:23:16 -0500

    update add data

--- artifacts/sql/3-add_data.sql
+++ artifacts/sql/3-add_data.sql
@@ -4,12 +4,12 @@ INSERT INTO tutorials_tbl
   VALUES
   ("Learn PHP", "John Poul", NOW());
 
-mysql> INSERT INTO tutorials_tbl
+INSERT INTO tutorials_tbl
   (tutorial_title, tutorial_author, submission_date)
   VALUES
   ("Learn MySQL", "Abdul S", NOW());
 
-mysql> INSERT INTO tutorials_tbl
+INSERT INTO tutorials_tbl
   (tutorial_title, tutorial_author, submission_date)
   VALUES
   ("JAVA Tutorial", "Sanjay", '2007-05-06');
